c92c5d4aa9ebc66b10ba378b889190862c3a3dc6
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-02 18:22:37 +0300

    Start script fix.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const serveStatic = require("serve-static");
 const path = require('path');
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'public')));
 const port = process.env.PORT || 80;
 app.listen(port);

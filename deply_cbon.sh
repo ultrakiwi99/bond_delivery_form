bdf8e2d70903d66d4160a0a2187019d5493e681a
harry <tsol@tsol.spb.ru>
2020-04-10 20:18:18 +0300

    This is kostyl to avoid android keyboard cover up whole input form       when run from inside the application

---
+++ deply_cbon.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -d dist_cbon ]; then
+	echo "removing old dist_cbon"
+	rm -r dist_cbon
+fi
+
+cp -r dist/ dist_cbon
+cd dist_cbon
+
+sed -i -e 's/href=\//href=\/delivery\//g' index.html
+sed -i -e 's/src=\//src=\/delivery\//g' index.html
+
+scp -r * root@192.168.25.9:/www/coffeebon/delivery/
+
+if [ -d dist_cbon ]; then
+	echo "removing old dist_cbon"
+	rm -r dist_cbon
+fi
+
+

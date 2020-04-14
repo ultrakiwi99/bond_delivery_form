631ee4f67169e8c0864e42d7863b1a1d104623a5
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-14 20:05:35 +0300

    Убрал тестовыв данные.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -94,17 +94,13 @@
                             ],
                             modifiers: [
                                 {name: 'Взбитые сливки', price: 29, selected: true, comment: ''},
-                                {name: 'Маршмеллоу белые (20 гр)', price: 29, selected: true, comment: ''},
+                                {name: 'Маршмеллоу белые (20 гр)', price: 29, selected: false, comment: ''},
                                 {name: 'Маршмеллоу цветные (20 гр)', price: 29, selected: false, comment: ''},
                                 {name: 'Доп. эспрессо', price: 39, selected: false, comment: ''},
                                 {
                                     name: 'Сироп',
                                     price: 29,
-                                    selected: false,
-                                    variants: {
-                                        selected: [],
-                                        list: ['Один', 'Два']
-                                    }
+                                    selected: false
                                 }
                             ],
                             visible: false

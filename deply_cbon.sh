1c24f503b0593bbf48bcf7e3694bb8440200209a
harry <tsol@tsol.spb.ru>
2020-04-24 02:49:11 +0300

    деплой изменен

--- deply_cbon.sh
+++ deply_cbon.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+HOST=192.168.25.9:/www/coffeebon/delivery/
+HOST=shop.coffeebon.ru:/www/delivery/
+
+echo Upload to ${HOST}
+
 if [ -d dist_cbon ]; then
 	echo "removing old dist_cbon"
 	rm -r dist_cbon
@@ -11,7 +16,7 @@ cd dist_cbon
 sed -i -e 's/href=\//href=\/delivery\//g' index.html
 sed -i -e 's/src=\//src=\/delivery\//g' index.html
 
-scp -r * root@192.168.25.9:/www/coffeebon/delivery/
+scp -r * root@${HOST}
 
 cd ..
 

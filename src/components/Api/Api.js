9bc580f82b81043cd6b9c0a1c8e0e799fe319954
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-05-02 14:58:06 +0300

    Доработки.

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -4,7 +4,7 @@ axios.defaults.timeout = 1800;
 
 export default class Api {
     constructor() {
-        this.baseUrl = 'http://portal.coffeebon.ru:8084/api'
+        this.baseUrl = 'http://localhost:8000/api'
     }
 
     sendOrder(client, store, cart) {

74e01d589ec4fa41873dcd653454c7a50b6e675a
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-05-06 18:14:12 +0300

    Фиксы css.

--- src/components/Menu/Modifiers/ModifiersList.vue
+++ src/components/Menu/Modifiers/ModifiersList.vue
@@ -1,25 +1,25 @@
 <template>
     <section class="menu-mods">
         <span class="secondary-text">Добавить</span>
-        <slot/>
+        <slot />
     </section>
 </template>
 
 <style lang="scss" scoped>
-    .mod-select {
+.mod-select {
+    display: flex;
+    align-items: center;
+    margin-left: 0;
+
+    .mod-details {
+        width: 100%;
         display: flex;
-        align-items: center;
-        margin-left: 0;
+        justify-content: flex-start;
+        align-items: baseline;
 
-        .mod-details {
-            width: 100%;
-            display: flex;
-            justify-content: start;
-            align-items: baseline;
-
-            .mod-price {
-                margin-left: 1rem;
-            }
+        .mod-price {
+            margin-left: 1rem;
         }
     }
+}
 </style>
